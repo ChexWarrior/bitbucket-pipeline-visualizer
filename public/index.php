d1d19c180d02d481b2d0ec27e1a9074cb156ebe9
Andrew Lehman <aplehm@gmail.com>
2020-11-02 20:50:44 -0500

    Load twig

--- public/index.php
+++ public/index.php
@@ -6,10 +6,13 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$loader = new \Twig\Loader\FilesystemLoader('../src/templates');
+$twig = new \Twig\Environment($loader);
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($twig) {
+    $html = $twig->render('home.twig');
+    $response->getBody()->write($html);
     return $response;
 });
 
