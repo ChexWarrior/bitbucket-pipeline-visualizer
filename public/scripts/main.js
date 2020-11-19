3580a1b8ee4ca45d0deb2423f37d8420ac21b394
Andrew Lehman <aplehm@gmail.com>
2020-11-18 19:06:02 -0500

    Add click event to reveal entire pipeline

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,9 @@
 const pipelineSection = document.querySelector('.pipelines');
-// pipelineSection.addEventListener('htmx:afterSettle', function (evt) {
-  const infoCards = pipelineSection.querySelectorAll('.pipeline-info');
-  infoCards.forEach((element) => {
-    const steps = element.querySelector('.pipeline-steps');
-    element.addEventListener('click', function (e) {
-      steps.classList.toggle('hidden');
-    })
+pipelineSection.addEventListener('htmx:afterSettle', (evt) => {
+  const pipelines = pipelineSection.querySelectorAll('.pipeline-info');
+  pipelines.forEach((elem, key) => {
+    elem.addEventListener('click', (evt) => {
+      elem.querySelector('.pipeline-steps').classList.toggle('is-hidden');
+    });
   });
-// });
+});
