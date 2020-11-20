2a4f478972642d29e7ada0db211dc42dda3877c1
Andrew Lehman <aplehm@gmail.com>
2020-11-19 22:59:56 -0500

    Hide title icon when expanding steps

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,9 +3,11 @@ pipelineSection.addEventListener('htmx:afterSettle', (evt) => {
   const pipelines = pipelineSection.querySelectorAll('.pipeline-info');
   pipelines.forEach((elem, key) => {
     elem.addEventListener('click', (evt) => {
+      const titleIcon = elem.querySelector('.title svg');
       const steps = elem.querySelector('.pipeline-steps');
       steps.classList.toggle('active');
       steps.classList.toggle('inactive');
+      titleIcon.classList.toggle('is-hidden');
     });
   });
 });
